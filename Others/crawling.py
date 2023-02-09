a265163e9825aed853c0dd33d26fd1fa769bb95f
qqaazz0222 <qqaazz0222@naver.com>
2023-02-09 21:35:32 +0900

    Update crawling.py

--- Others/crawling.py
+++ Others/crawling.py
@@ -1,13 +1,34 @@
-import requests
-from bs4 import BeautifulSoup
+import urllib.request
+from selenium.common.exceptions import NoSuchElementException
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 
-url = "https://m.blog.naver.com/alens82/220719833209"
 
-response = requests.get(url)
+# 웹드라이버 설정
+options = webdriver.ChromeOptions()
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option("useAutomationExtension", False)
 
-if response.status_code == 200:  # 정상 연결시
-    soup = BeautifulSoup(response.text, 'html.parser')  # 수프 text 형태로 만들기
-    text = str(soup.find(id='postViewArea'))  # 본문 내용만 담긴 css 코드 뽑아내기
-    print(text)
+# 정보입력
+client_id = "*********************" # 발급받은 id 입력
+client_secret = "*************" # 발급받은 secret 입력 
+quote = input("검색어를 입력해주세요.: ") #검색어 입력받기
+encText = urllib.parse.quote(quote)
+display_num = input("검색 출력결과 갯수를 적어주세요.(최대100, 숫자만 입력): ") #출력할 갯수 입력받기
+url = "https://openapi.naver.com/v1/search/blog?query=" + encText +"&display="+display_num# json 결과
+# url = "https://openapi.naver.com/v1/search/blog.xml?query=" + encText # xml 결과
+request = urllib.request.Request(url)
+request.add_header("X-Naver-Client-Id",client_id)
+request.add_header("X-Naver-Client-Secret",client_secret)
+response = urllib.request.urlopen(request)
+rescode = response.getcode()
+
+if(rescode==200):
+    response_body = response.read()
+    #print(response_body.decode('utf-8'))
 else:
-    print(response.status_code)
+    print("Error Code:" + rescode)
+
+body = response_body.decode('utf-8')
+body
